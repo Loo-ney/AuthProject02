d10cc2d9044431f92b0451a32a85c401c0417c5f
Loo-ney <noe.ralte@gmail.com>
2024-10-04 23:16:44 +0530

    reset password

--- server/router/route.js
+++ server/router/route.js
@@ -24,7 +24,7 @@ router.route('/createResetSession').get(controller.createResetSession)  // reset
 
 /** PUT METHODS */
 router.route('/updateuser').put(Auth, controller.updateUser) // is use to update the user profile 
-router.route('/resetPassword').put(controller.resetPassword) // use to reset password
+router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword) // use to reset password
 
 
 export default router
